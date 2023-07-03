2ebcd07714f1f7f4ae044f8c289e20d8d29b0a67
poloarol <paularol211@yhaoo.com>
2023-07-03 17:05:52 -0400

    Added the Fontawesome Icon, added the Trash, Declared the props type and passed the functional components

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FC } from "react";
 
-const Todo = () => {
-  return <div>Todo</div>;
+type Task = {
+  task: string;
+};
+type TodoProps = {
+  task: Task;
+};
+
+const Todo: FC<TodoProps> = (props) => {
+  const { task } = props;
+  return (
+    <div className="Todo">
+      <p>{task.task}</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
