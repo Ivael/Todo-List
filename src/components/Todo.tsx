18215d950804c22578b3b9b8e0be373b021a3d64
poloarol <paularol211@yhaoo.com>
2023-07-04 12:24:06 -0400

    Added the type Task, Added the togglecomplete function

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,18 +3,29 @@ import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FC } from "react";
 
-type Task = {
-  task: string;
+export type Task = {
+  text: string;
+  id: string;
+  completed: boolean;
 };
+
 type TodoProps = {
   task: Task;
+  toggleComplete: (id: string) => void;
 };
 
 const Todo: FC<TodoProps> = (props) => {
-  const { task } = props;
+  const { task, toggleComplete } = props;
   return (
     <div className="Todo">
-      <p>{task.task}</p>
+      {
+        <p
+          onClick={() => toggleComplete(task.id)}
+          className={task.completed ? "completed" : ""}
+        >
+          {task.text}
+        </p>
+      }
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
         <FontAwesomeIcon icon={faTrash} />
