499af98674e2303b29ece878630ce3fd71f2d2f2
poloarol <paularol211@yhaoo.com>
2023-07-04 12:24:26 -0400

    Added a newTask object

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,14 +1,23 @@
 import { FC, useState } from "react";
+import { Task } from "./Todo";
+import { v4 as uuid } from "uuid";
 
 interface TodoFormProps {
-  onAddTodo: (Value: string) => void;
+  onAddTodo: (Value: Task) => void;
 }
 const TodoForm: FC<TodoFormProps> = (props) => {
   const { onAddTodo } = props;
   const [value, setValue] = useState("");
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    onAddTodo(value);
+
+    const newTask: Task = {
+      id: uuid(),
+      text: value,
+      completed: false,
+    };
+
+    onAddTodo(newTask);
     setValue("");
   };
 
