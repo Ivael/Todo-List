31bb9e2c7457623ae3315c8d7f2bc2a8b82a4e8a
poloarol <paularol211@yhaoo.com>
2023-07-04 12:21:20 -0400

    Edit from file

--- src/components/EditTodoForm.tsx
+++ src/components/EditTodoForm.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { FC, useState } from "react";
+import { Task } from "./Todo";
+import { v4 as uuid } from "uuid";
 
-const EditTodoForm = () => {
-  return <div>EditTodoForm</div>;
+interface EditTodoFormProps {
+  editTodo: (Value: Task, id: Task) => void;
+}
+const EditTodoForm: FC<EditTodoFormProps> = (props) => {
+  const { editTodo } = props;
+  const [value, setValue] = useState("");
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const newTask: Task = {
+      id: uuid(),
+      text: value,
+      completed: false,
+    };
+
+    editTodo(newTask);
+    setValue("");
+  };
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Update Task"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Update task
+      </button>
+    </form>
+  );
 };
 
 export default EditTodoForm;
